e5e48d26d14f7908cc82f6356afe2c0e4229648b
zhoupengxiang <zhoupengxiang255@163.com>
2025-12-06 21:31:09 +0800

    12.6 套餐分页查询以及增删改 套餐起售 营业状态设置功能 补充了缺失的菜品起售停售状态设置(记得写过,莫名不见)

--- sky-server/src/main/java/com/sky/service/impl/DishService.java
+++ sky-server/src/main/java/com/sky/service/impl/DishService.java
@@ -31,4 +31,12 @@ public interface DishService {
      */
     List<Dish> list(Long categoryId);
 
+    /**
+     * 菜品起售停售
+     * @param status
+     * @param id
+     */
+    void startOrStop(Integer status, Long id);
+
+
 }
